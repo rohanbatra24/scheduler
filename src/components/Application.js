b7d782a512bc4e5eb70dacea8f1c57202acff480
Rohan Batra <rohanbatra24@gmail.com>
2020-04-15 21:00:13 -0400

    conbined state

--- src/components/Application.js
+++ src/components/Application.js
@@ -58,9 +58,15 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [ day, setDay ] = useState('Monday');
+  const [ state, setState ] = useState({
+    day: 'Monday',
+    days: [],
+    appointments: {}
+  });
 
-  const [ days, setDays ] = useState([]);
+  const setDay = (day) => setState({ ...state, day });
+
+  const setDays = (days) => setState({ ...state, days });
 
   useEffect(() => {
     axios.get(`/api/days`).then((response) => {
@@ -78,7 +84,7 @@ export default function Application(props) {
         <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler" />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
       </section>
