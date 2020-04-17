fa854accc4cab1209d4f1f77de20a437d9ae52ee
Rohan Batra <rohanbatra24@gmail.com>
2020-04-17 12:56:43 -0400

    fixed back function and cancel on form working

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -41,7 +41,7 @@ export default function Appointment(props) {
     return (
       <article className="appointment">
         <Header time={props.time} />
-        <Form interviewers={props.interviewers} />
+        <Form onCancel={back} interviewers={props.interviewers} />
       </article>
     );
   }
